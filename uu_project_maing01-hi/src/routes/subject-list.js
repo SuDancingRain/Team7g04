67f02f2270fd6befaca3b37293393060f82092bd
Vincenc Nguyen <su.dancing.rain@gmail.com>
2022-02-22 18:22:36 +0100

    Update subject-list.js

--- uu_project_maing01-hi/src/routes/subject-list.js
+++ uu_project_maing01-hi/src/routes/subject-list.js
@@ -1,7 +1,7 @@
 //@@viewOn:imports
 import UU5 from "uu5g04";
 import "uu5g04-bricks";
-import { createVisualComponent,useState,useDataList} from "uu5g04-hooks";
+import { createVisualComponent, useState, useDataList } from "uu5g04-hooks";
 import Uu5Tiles from "uu5tilesg02";
 import "uu_plus4u5g01-bricks";
 import Config from "./config/config.js";
@@ -17,184 +17,183 @@ const STATICS = {
   //@@viewOff:statics
 };
 
-export const SubjectList = 
-createVisualComponent({
-  ...STATICS,
+export const SubjectList =
+  createVisualComponent({
+    ...STATICS,
 
-  //@@viewOn:propTypes
-  propTypes: {},
-  //@@viewOff:propTypes
+    //@@viewOn:propTypes
+    propTypes: {},
+    //@@viewOff:propTypes
 
-  //@@viewOn:defaultProps
-  defaultProps: {},
-  //@@viewOff:defaultProps
+    //@@viewOn:defaultProps
+    defaultProps: {},
+    //@@viewOff:defaultProps
 
-  render(props) {
-    //@@viewOn:private
+    render(props) {
+      //@@viewOn:private
 
-    const [selectedSubject, setSelectedSubject] = useState(null);
-    const [subjectToDelete, setSubjectToDelete] = useState(null);
+      const [selectedSubject, setSelectedSubject] = useState(null);
+      const [subjectToDelete, setSubjectToDelete] = useState(null);
 
-    const subjectListData = useDataList({
-      handlerMap: {
-        load: Calls.Subject.list,
-        createItem: Calls.Subject.create,
-      },
-      itemHandlerMap: {
-        update: Calls.Subject.update,
-        delete: Calls.Subject.delete,
-      },
-      initialDtoIn: {},
-    });
-
-
-    const assignmentAvailableTags = [];
-    if (props.data) {
-      props.data.forEach((assignment) => {
-        assignmentAvailableTags.push({
-          value: assignment.data.id,
-          content: assignment.data.name,
-        });
+      const subjectListData = useDataList({
+        handlerMap: {
+          load: Calls.Subject.list,
+          createItem: Calls.Subject.create,
+        },
+        itemHandlerMap: {
+          update: Calls.Subject.update,
+          delete: Calls.Subject.delete,
+        },
+        initialDtoIn: {},
       });
-    }
 
 
-    //@@viewOff:private
+      const assignmentAvailableTags = [];
+      if (props.data) {
+        props.data.forEach((assignment) => {
+          assignmentAvailableTags.push({
+            value: assignment.data.id,
+            content: assignment.data.name,
+          });
+        });
+      }
 
-    //@@viewOn:interface
 
-    function handleCreateSubject(newSubjectData) {
-      return subjectListData.handlerMap.createItem(newSubjectData);
-    }
+      //@@viewOff:private
 
-    function handleUpdateSubject(updatedSubjectData) {
-      return selectedSubject.handlerMap.update(updatedSubjectData);
-    }
+      //@@viewOn:interface
 
-    async function handleSubjectDelete() {
-      await subjectToDelete.handlerMap.delete({ id: subjectToDelete.data.id });
-      setSubjectToDelete(null);
-      window.location.reload();
-    }
-    //@@viewOff:interface
+      function handleCreateSubject(newSubjectData) {
+        return subjectListData.handlerMap.createItem(newSubjectData);
+      }
 
-    //@@viewOn:render
-    const className = Config.Css.css``;
-    const attrs = UU5.Common.VisualComponent.getAttrs(props, className);
-    const currentNestingLevel = UU5.Utils.NestingLevel.getNestingLevel(props, STATICS);
+      function handleUpdateSubject(updatedSubjectData) {
+        return selectedSubject.handlerMap.update(updatedSubjectData);
+      }
 
-    function getCollumns() {
-      return [
-        {
-          header: <UU5.Bricks.Lsi lsi={Lsi.name} />,
-          sorterKey: "nameAsc",
-          cell: (cellProps) => cellProps.data.data.name,
+      async function handleSubjectDelete() {
+        await subjectToDelete.handlerMap.delete({ id: subjectToDelete.data.id });
+        setSubjectToDelete(null);
+        window.location.reload();
+      }
+      //@@viewOff:interface
 
-        },
-        {
-          header: <UU5.Bricks.Lsi lsi={Lsi.degree}/>,
-          cell: (cellProps) => cellProps.data.data.degree,
-        },
-        {
-          header: <UU5.Bricks.Lsi lsi={Lsi.language}/>,
-          cell: (cellProps) => cellProps.data.data.language,
-        },
+      //@@viewOn:render
+      const className = Config.Css.css``;
+      const attrs = UU5.Common.VisualComponent.getAttrs(props, className);
+      const currentNestingLevel = UU5.Utils.NestingLevel.getNestingLevel(props, STATICS);
 
-        {
-          cell: (cellProps) => {
-            if (cellProps.data.state.includes("pending")) {
-              return <UU5.Bricks.Loading />
-            } else {
-              return (
-                <>
-                  <UU5.Bricks.Button
-                    colorSchema="blue"
-                    onClick={() => { UU5.Environment.getRouter().setRoute("subjectDetail", { id: cellProps.data.data.id }) }}
-                  >
-                    <UU5.Bricks.Icon
-                      icon="mdi-magnify"
-                    />
-                  </UU5.Bricks.Button>
-                  <UU5.Bricks.Button
-                    colorSchema="blue"
-                    onClick={() => setSelectedSubject(cellProps.data)}
-                  >
-                    <UU5.Bricks.Icon icon="mdi-pencil" />
-                  </UU5.Bricks.Button>
-                  <UU5.Bricks.Button
-                    colorSchema="red"
-                    onClick={() => setSubjectToDelete(cellProps.data)}
-                  >
-                    <UU5.Bricks.Icon
-                      icon="mdi-close"
-                    />
-                  </UU5.Bricks.Button>
-                </>
-              );
-            }
+      function getCollumns() {
+        return [
+          {
+            header: <UU5.Bricks.Lsi lsi={Lsi.name} />,
+            sorterKey: "nameAsc",
+            cell: (cellProps) => cellProps.data.data.name,
+
           },
-        },
-      ];
-    }
-    const Filter =[
+          {
+            header: <UU5.Bricks.Lsi lsi={Lsi.degree} />,
+            cell: (cellProps) => cellProps.data.data.degree,
+          },
+          {
+            header: <UU5.Bricks.Lsi lsi={Lsi.language} />,
+            cell: (cellProps) => cellProps.data.data.language,
+          },
 
-    ]
-    const Sorter =[
-      
-    ]
-    return currentNestingLevel ? (
-      <div {...attrs}>
-        {
-          selectedSubject && (
+          {
+            cell: (cellProps) => {
+              if (cellProps.data.state.includes("pending")) {
+                return <UU5.Bricks.Loading />
+              } else {
+                return (
+                  <>
+                    <UU5.Bricks.Button
+                      colorSchema="blue"
+                      onClick={() => { UU5.Environment.getRouter().setRoute("subjectDetail", { id: cellProps.data.data.id }) }}
+                    >
+                      <UU5.Bricks.Icon
+                        icon="mdi-magnify"
+                      />
+                    </UU5.Bricks.Button>
+                    <UU5.Bricks.Button
+                      colorSchema="blue"
+                      onClick={() => setSelectedSubject(cellProps.data)}
+                    >
+                      <UU5.Bricks.Icon icon="mdi-pencil" />
+                    </UU5.Bricks.Button>
+                    <UU5.Bricks.Button
+                      colorSchema="red"
+                      onClick={() => setSubjectToDelete(cellProps.data)}
+                    >
+                      <UU5.Bricks.Icon
+                        icon="mdi-close"
+                      />
+                    </UU5.Bricks.Button>
+                  </>
+                );
+              }
+            },
+          },
+        ];
+      }
+      const Filter = [
+
+      ]
+      const Sorter = [
+
+      ]
+      return currentNestingLevel ? (
+        <div {...attrs}>
+          {
+            selectedSubject && (
+              <UU5.Bricks.Modal
+                header={<UU5.Bricks.Lsi lsi={props.selectedSubject?.id ? Lsi.updateSubject : Lsi.createSubject} />}
+                shown={!!selectedSubject}
+                onClose={() => setSelectedSubject(null)}
+              >
+                <SubjectForm
+                  selectedSubject={selectedSubject.data}
+                  setSelectedSubject={setSelectedSubject}
+                  handleCreateSubject={handleCreateSubject}
+                  handleUpdateSubject={handleUpdateSubject}
+                />
+              </UU5.Bricks.Modal>
+            )
+          }
+
+          {subjectToDelete && (
             <UU5.Bricks.Modal
-              header={<UU5.Bricks.Lsi lsi={props.selectedSubject?.id ? Lsi.updateSubject : Lsi.createSubject} />}
-              shown={!!selectedSubject}
-              onClose={() => setSelectedSubject(null)}
+              header={"Confirm Subject Deletion"}
+              shown={true}
+              onClose={() => setSubjectToDelete(null)}
             >
-              <SubjectForm
-                selectedSubject={selectedSubject.data}
-                setSelectedSubject={setSelectedSubject}
-                handleCreateSubject={handleCreateSubject}
-                handleUpdateSubject={handleUpdateSubject}
-              />
+              <div className={"center uu5-common-padding-s"}>
+                <UU5.Bricks.Button onClick={() => setSubjectToDelete(null)}>
+                  Refuse
+                </UU5.Bricks.Button>
+                {""}
+                <UU5.Bricks.Button colorSchema={"red"} onClick={handleSubjectDelete}>
+                  Confirm
+                </UU5.Bricks.Button>
+              </div>
             </UU5.Bricks.Modal>
           )
-        }
+          }
 
-        {subjectToDelete && (
-          <UU5.Bricks.Modal
-            header={"Confirm Subject Deletion"}
-            shown={true}
-            onClose={() => setSubjectToDelete(null)}
-          >
-            <div className={"center uu5-common-padding-s"}>
-              <UU5.Bricks.Button onClick={() => setSubjectToDelete(null)}>
-                Refuse
+          <UU5.Bricks.Container>
+            <Uu5Tiles.ControllerProvider data={subjectListData.data || []} filters={Filter} >
+              <UU5.Bricks.Button colorSchema={"green"} onClick={() => setSelectedSubject({ data: {} })}>
+                <UU5.Bricks.Icon icon={"mdi-plus"} />
+                <UU5.Bricks.Lsi lsi={Lsi.create} />
               </UU5.Bricks.Button>
-              {""}
-              <UU5.Bricks.Button colorSchema={"red"} onClick={handleSubjectDelete}>
-                Confirm
-              </UU5.Bricks.Button>
-            </div>
-          </UU5.Bricks.Modal>
-        )
-        }
-        
-        <UU5.Bricks.Container>
-          <Uu5Tiles.ControllerProvider data={subjectListData.data || []} filters={Filter} >
-        <UU5.Bricks.Button colorSchema={"green"} onClick={()=> setSelectedSubject({data: {} })}>
-          <UU5.Bricks.Icon icon={"mdi-plus"} />
-          <UU5.Bricks.Lsi lsi={Lsi.create} />
-        </UU5.Bricks.Button>
-        <Uu5Tiles.FilterBar />
-            <Uu5Tiles.List columns={getCollumns()} rowAlignment="center" rowHeight={150} />
-          </Uu5Tiles.ControllerProvider>
-        </UU5.Bricks.Container>
-      </div>
-    ) : null;
-    //@@viewOff:render
-  },
-})
-;
+              <Uu5Tiles.List columns={getCollumns()} rowAlignment="center" rowHeight={150} />
+            </Uu5Tiles.ControllerProvider>
+          </UU5.Bricks.Container>
+        </div>
+      ) : null;
+      //@@viewOff:render
+    },
+  })
+  ;
 
 export default SubjectList;
